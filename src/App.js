01d15b9d82fa6266704735ca16ff6d637e05a0c4
I-am-Miguel <miguelwelligton@gmail.com>
2018-02-19 20:04:46 -0300

    Cadastro de autores, atualizando listagem

--- src/App.js
+++ src/App.js
@@ -10,16 +10,59 @@ class App extends Component {
   constructor(){
     super();
     this.state = {
-      lista: [{}]
+      nome: "",
+      email: "",
+      senha: "",
+      lista: []
     }
+    this.enviarForm = this.enviarForm.bind(this)
+    this.setNome = this.setNome.bind(this)
+    this.setEmail = this.setEmail.bind(this)
+    this.setSenha = this.setSenha.bind(this)
   }
 
   componentDidMount(){    
     $.ajax({
       url: "http://cdc-react.herokuapp.com/api/autores",
       dataType: "json",
-      success:((resposta) =>{this.setState({lista:resposta})}).bind(this)
+      success:(resposta => {this.setState({lista:resposta})}).bind(this)
     });    
+  }
+
+  enviarForm(evento){
+    //Preserva o estado dá página, não propaga o evento
+    evento.preventDefault()
+    
+    $.ajax({
+      url: "http://cdc-react.herokuapp.com/api/autores",
+      contentType: "application/json",
+      dataType: "json",
+      type:"post",
+      data: JSON.stringify(
+        {nome: this.state.nome,
+        email: this.state.email,
+        senha: this.state.senha
+      }),
+      success: (function(resposta){
+        this.setState({lista:resposta})
+        }.bind(this)
+      ),
+      error:function(resposta){
+        console.log(resposta)
+      }
+    });
+  }
+
+  setNome(evento){
+    this.setState({nome:evento.target.value})
+  }
+
+  setEmail(evento){
+    this.setState({email:evento.target.value}) 
+  }
+
+  setSenha(evento){
+    this.setState({senha:evento.target.value})
   }
 
   render() {    
@@ -47,18 +90,18 @@ class App extends Component {
           </div>
           <div className="content" id="content">
             <div className="pure-form pure-form-aligned">
-              <form className="pure-form pure-form-aligned">
+              <form className="pure-form pure-form-aligned" onSubmit = {this.enviarForm} method="post">
                 <div className="pure-control-group">
                   <label htmlFor="nome">Nome</label> 
-                  <input id="nome" type="text" name="nome" value=""  />                  
+                  <input id="nome" type="text" name="nome" value= {this.state.nome} onChange= {this.setNome} />              
                 </div>
                 <div className="pure-control-group">
                   <label htmlFor="email">Email</label> 
-                  <input id="email" type="email" name="email" value=""  />                  
+                  <input id="email" type="email" name="email" value= {this.state.email} onChange= {this.setEmail}/>
                 </div>
                 <div className="pure-control-group">
                   <label htmlFor="senha">Senha</label> 
-                  <input id="senha" type="password" name="senha"  />                                      
+                  <input id="senha" type="password" name="senha" value= {this.state.senha} onChange= {this.setSenha}/>
                 </div>
                 <div className="pure-control-group">                                  
                   <label></label> 
@@ -83,7 +126,7 @@ class App extends Component {
                           <td>{autor.nome}</td>
                           <td>{autor.email}</td>
                         </tr>
-                        )
+                      )
                     })
                   }
                 </tbody>
